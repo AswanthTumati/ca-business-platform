a5444d356191e9f28f2f210a4f1159c2760d6383
aswanthtumati <aswanthtumati@gmail.com>
2026-07-15 13:40:53 +0530

    chore: add ntlify spa redirects

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./assets/styles/variables.css";
-import "./assets/styles/global.css";
-import "./assets/styles/utilities.css";
+
 
 import App from './App.jsx'
 
